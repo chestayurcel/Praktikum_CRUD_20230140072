3314770aa9e37a8199e2c3f042fdb04255f58416
Yurcell <chestayurcel@gmail.com>
2026-03-02 10:14:28 +0700

    Menambahkan code pada UserMapper

--- src/main/java/com/be/praktikum1/mapper/UserMapper.java
+++ src/main/java/com/be/praktikum1/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.be.praktikum1.mapper;
 
+import com.be.praktikum1.model.dto.UserDto;
+import com.be.praktikum1.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+    UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
+    UserDto toUserDtoData (User user);
 }
